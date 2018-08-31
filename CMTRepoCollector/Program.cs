709be65c634573ea5fbe1f06e767cbc89d40b985
Ciaran McKenna <Ciaran McKenna@cw-data.local>
2018-08-31 23:05:17 +0100

    Add project files.

---
+++ CMTRepoCollector/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using Microsoft.VisualBasic;
+
+
+namespace CMTRepoCollector
+{
+    class Program
+    {
+        public static object FileSystem { get; private set; }
+
+        static void Main(string[] args)
+        {
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Wow6432Node\Craneware\ACMPT\NetworkConfig\");
+            string sourcePath = (string)registryKey.GetValue("SharedPath");
+
+            string targetPath = @"C:\TEST";
+            string targetDatabase = targetPath + @"\Databases";
+            string targetRepo = targetPath + @"\Repository";
+
+            Directory.CreateDirectory(targetPath);
+            Directory.CreateDirectory(targetDatabase);
+            Directory.CreateDirectory(targetRepo);
+
+            var extensions = new[] { ".dat", ".loc", ".info" };
+
+            var files = (from file in Directory.EnumerateFiles(sourcePath)
+                         where extensions.Contains(Path.GetExtension(file), StringComparer.InvariantCultureIgnoreCase)
+                         select new
+                         {
+                             Source = file,
+                             Destination = Path.Combine(targetPath, Path.GetFileName(file))
+                         });
+
+            foreach (var file in files)
+            {
+                File.Copy(file.Source, file.Destination);
+            }
+
+            string sourceDatabase = sourcePath + @"\Databases";
+            string sourceRepo = sourcePath + @"\Repository";
+
+
+            DirectoryInfo dir = new DirectoryInfo(sourceDatabase);
+            FileInfo[] folder = dir.GetFiles();
+            foreach (FileInfo file in folder)
+                {
+                    string pathTo = targetDatabase + @"\" + file;
+                    file.CopyTo(pathTo);
+                }
+
+            new Microsoft.VisualBasic.Devices.Computer().FileSystem.CopyDirectory(sourceRepo, targetRepo);
+
+            //FileSystem.CopyDirectory(srce, dest,)
+            //Microsoft.VisualBasic.FileIO.UIOption.AllDialogs,
+            //Microsoft.VisualBasic.FileIO.UICancelOption.DoNothing);
+
+        }
+    }
+}
---
+++ CopyFolders/CopyFolders.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CMTRepoCollector
+{
+    public class CopyFolders
+    {
+        public void DirectoryCopy(string sourceDirName, string destDirName)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+
+            // Get the files in the directory and copy them to the new location.
+            //FileInfo[] files = dir.GetFiles();
+
+            //foreach (FileInfo file in files)
+            //{
+            //    string temppath = Path.Combine(destDirName, file.Name);
+            //    file.CopyTo(temppath, false);
+            //}
+        }
+    }
+}
